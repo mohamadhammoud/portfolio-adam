7782e73f2adf84cadc2f70985cb7451ce5518e0d
aboudjem <boudjemaa.adam@gmail.com>
2023-08-12 17:48:56 +0400

    test with imports

--- src/components/organisms/Main.tsx
+++ src/components/organisms/Main.tsx
@@ -15,6 +15,7 @@ import Fantom from '../atoms/icons/Fantom';
 import Tokeny from '../atoms/icons/Tokeny';
 import Arcelormittal from '../atoms/icons/Arcelormittal';
 import Biconomy from '../atoms/icons/Biconomy';
+import adamImage from 'assets/images/core/adam-blockchain-web3-developer-solidity.webp';
 
 function Main() {
   return (
@@ -1202,7 +1203,7 @@ function Main() {
               </div>
               <div className="mx-auto">
                 <img
-                  src="assets/images/core/adam-blockchain-web3-developer-solidity.webp"
+                  src={adamImage}
                   className="w-full"
                   alt="adam-boudjemaa-blockchain-developer"
                 />
