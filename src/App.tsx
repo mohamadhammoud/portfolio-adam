835b0824aa9e4bbf729f85d66f2693d4e9d0a602
aboudjem <boudjemaa.adam@gmail.com>
2023-08-22 14:25:15 +0700

    remove header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import Header from './components/organisms/Header';
 import Main from './components/organisms/Main';
 import Footer from './components/organisms/Footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
       <Main />
       <Footer />
     </div>
