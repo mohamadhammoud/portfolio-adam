6a6e226e0907bf723529bb2b3366a464e51d8634
Adam Boudj <boudjemaa.adam@gmail.com>
2023-08-24 16:36:15 +0400

    Update changes (#1)
    * Remove purple-version
    
    * remove new version folder
    
    * ds store
    
    * Delete .DS_Store
    
    * remove unused files
    
    * fix lint errors
    
    * optimize pic size
    
    * Fix footer telegram
    
    * optimize img size
    
    * optimizing size and removing unused files
    
    * lint fix
    
    * fix style
    
    * replace png with webp
    
    * add build to gitignore
    
    * test with imports
    
    * import webp
    
    * revert changes
    
    * fix lint
    
    * revert
    
    * update to webp format
    
    * add ignore rules
    
    * delete build
    
    * update size of main image
    
    * optimization and fix
    
    * optimize
    
    * optimize accessibility, contrast,a aria label
    
    * removed unused link
    
    * removing scripts
    
    * add sitemap, optimize index.html seo, remove unused files
    
    * preload
    
    * update link
    
    * remove header
    
    * add space
    
    * seo robots sitemap and index updated
    
    * Save
    
    ---------
    
    Co-authored-by: Adam Boudjiz <adam@Adams-MacBook-Pro.local>

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from './components/organisms/Main';
+import Footer from './components/organisms/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
